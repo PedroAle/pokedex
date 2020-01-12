f1e871f33409fcd3941386bfb1383d20e0f12265
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2020-01-11 23:14:19 -0400

    Frontend Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'materialProyect';
+
+
+  displayedColumns3: string[] = ['action','name'];
+
+  value: string;
+
+  pokemon: any = {};
+  
+  dataSource;
+
+  showDetail: boolean = false;
+
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit() {
+    
+    this.get_characters();
+    
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (filterValue = ''){
+      this.value = ''
+    }
+  }
+
+  async get_characters(){
+    await this.httpClient.get('https://pokeapi.co/api/v2/pokemon/').subscribe((res)=>{
+        this.dataSource = new MatTableDataSource(res['results'])
+        //console.log("data: ", this.data)
+        console.log(this.dataSource)
+        this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  async get_detail(url){
+    console.log(url)
+    
+    await this.httpClient.get(url).subscribe((res)=>{
+      console.log(res)
+      this.pokemon = res;
+      //console.log("data: ", this.data)
+      console.log(this.pokemon)
+      this.showDetail = true;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +20,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatRippleModule,
+    MatCardModule,
+    MatIconModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  exports: [
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
